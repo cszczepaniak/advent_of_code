803c99e3745c1825b3b9d9ef108954b6a6717961
cszczepaniak <cszczepaniak@gmail.com>
2023-01-06 18:28:25 -0600

    make it _blazingly fast_

--- 2022/src/day6.rs
+++ 2022/src/day6.rs
@@ -1,73 +1,28 @@
 pub fn part_one(input: &str) -> anyhow::Result<usize> {
-    find_marker(input, 4).ok_or(anyhow::anyhow!("didn't find a marker"))
+    find_marker(input.as_bytes(), 4).ok_or(anyhow::anyhow!("didn't find a marker"))
 }
 
 pub fn part_two(input: &str) -> anyhow::Result<usize> {
-    find_marker(input, 14).ok_or(anyhow::anyhow!("didn't find a marker"))
+    find_marker(input.as_bytes(), 14).ok_or(anyhow::anyhow!("didn't find a marker"))
 }
 
-fn find_marker(input: &str, n: usize) -> Option<usize> {
-    let mut u = Uniquer::new(n);
+fn find_marker(input: &[u8], length: usize) -> Option<usize> {
+    let mut idx = 0;
+    while let Some(slice) = input.get(idx..idx + length) {
+        let mut state = 0u32;
 
-    for (i, c) in input.chars().enumerate() {
-        if i >= n {
-            let leaving = input.as_bytes()[i - n] as char;
-            u.remove(leaving);
-        }
-        u.add(c);
-
-        if u.unique() {
-            return Some(i + 1);
+        if let Some(pos) = slice.iter().rposition(|byte| {
+            let bit_idx = byte % 32;
+            let ret = state & (1 << bit_idx) != 0;
+            state |= 1 << bit_idx;
+            ret
+        }) {
+            idx += pos + 1;
+        } else {
+            return Some(idx + length);
         }
     }
-
     None
-}
-
-struct Uniquer {
-    state: [usize; 26],
-    num_greater_than_one: usize,
-    n: usize,
-}
-
-impl Uniquer {
-    fn new(n: usize) -> Self {
-        Self {
-            state: [0; 26],
-            num_greater_than_one: 0,
-            n,
-        }
-    }
-
-    fn add(&mut self, c: char) {
-        let idx = c as usize - 'a' as usize;
-        if self.state[idx] == 1 {
-            self.num_greater_than_one += 1;
-        }
-        self.state[idx] += 1;
-    }
-
-    fn remove(&mut self, c: char) {
-        let idx = c as usize - 'a' as usize;
-        if self.state[idx] == 2 {
-            self.num_greater_than_one -= 1;
-        }
-        self.state[idx] -= 1;
-    }
-
-    fn unique(&self) -> bool {
-        if self.num_greater_than_one > 0 {
-            return false;
-        }
-        let mut sum = 0;
-        for count in self.state.iter() {
-            if count > &1 {
-                return false;
-            }
-            sum += count;
-        }
-        sum == self.n
-    }
 }
 
 #[cfg(test)]
@@ -90,15 +45,21 @@ mod tests {
 
     #[test]
     fn test_part_one_examples() {
-        assert_eq!(5, find_marker("bvwbjplbgvbhsrlpgdmjqwftvncz", 4).unwrap());
-        assert_eq!(6, find_marker("nppdvjthqldpwncqszvftbrmjlhg", 4).unwrap());
+        assert_eq!(
+            5,
+            find_marker("bvwbjplbgvbhsrlpgdmjqwftvncz".as_bytes(), 4).unwrap()
+        );
+        assert_eq!(
+            6,
+            find_marker("nppdvjthqldpwncqszvftbrmjlhg".as_bytes(), 4).unwrap()
+        );
         assert_eq!(
             10,
-            find_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4).unwrap()
+            find_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg".as_bytes(), 4).unwrap()
         );
         assert_eq!(
             11,
-            find_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4).unwrap()
+            find_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".as_bytes(), 4).unwrap()
         );
     }
 
@@ -106,9 +67,15 @@ mod tests {
     fn test_part_two_examples() {
         assert_eq!(
             19,
-            find_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14).unwrap()
+            find_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb".as_bytes(), 14).unwrap()
         );
-        assert_eq!(23, find_marker("bvwbjplbgvbhsrlpgdmjqwftvncz", 14).unwrap());
-        assert_eq!(23, find_marker("nppdvjthqldpwncqszvftbrmjlhg", 14).unwrap());
+        assert_eq!(
+            23,
+            find_marker("bvwbjplbgvbhsrlpgdmjqwftvncz".as_bytes(), 14).unwrap()
+        );
+        assert_eq!(
+            23,
+            find_marker("nppdvjthqldpwncqszvftbrmjlhg".as_bytes(), 14).unwrap()
+        );
     }
 }
